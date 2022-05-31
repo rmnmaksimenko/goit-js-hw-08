6ff57dac4f1887d9804044f8d33f42a06d5a2339
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-05-31 11:51:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
 
-formInput = document.querySelector('.feedback-form');
+form = document.querySelector('.feedback-form');
 
 const STORAGE_KEY = 'feedback-form-state';
 const storageForm = {};
@@ -9,17 +9,17 @@ const savedData = localStorage.getItem(`${STORAGE_KEY}`);
 if (savedData) {
   savedObject = JSON.parse(savedData);
   for (const key in savedObject) {
-    formInput[key].value = storageForm[key] = savedObject[key];
+    form[key].value = storageForm[key] = savedObject[key];
   }
 }
 
-formInput.addEventListener('input', throttle(onTextAreaInput, 500));
+form.addEventListener('input', throttle(onTextAreaInput, 500));
 
-formInput.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   console.log(storageForm);
   localStorage.removeItem(`${STORAGE_KEY}`);
-  formInput.reset();
+  form.reset();
   for (key in storageForm) {
     storageForm[key] = '';
   }
